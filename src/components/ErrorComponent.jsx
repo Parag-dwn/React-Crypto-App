17347578bc124b99bcc85302b68bf022f7e2c9c5
Parag-dwn <79037459+Parag-dwn@users.noreply.github.com>
2023-01-25 16:12:48 +0530

    crypto project complete

---
+++ src/components/ErrorComponent.jsx
@@ -0,0 +1,21 @@
+import { Alert, AlertIcon } from '@chakra-ui/react'
+import React from 'react'
+
+const ErrorComponent = ({message}) => {
+  return (
+    <Alert 
+        state={"error"} 
+        position={"fixed"} 
+        bottom={"4"} 
+        left={"50%"} 
+        w={"container.lg"}
+        >
+    <AlertIcon/>
+    {message}
+    
+    </Alert>
+  )
+}
+
+export default ErrorComponent
+
